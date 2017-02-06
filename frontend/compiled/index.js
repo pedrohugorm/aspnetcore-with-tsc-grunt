3d531c53d9023b07e874dab5bb58ea5523428cff
Pedro Matos <pedromatos@Pedros-MacBook-Pro.local>
2017-02-06 10:16:04 -0600

    Adding example_tasks.json

---
+++ frontend/compiled/index.js
@@ -0,0 +1,22 @@
+"use strict";
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var React = require("react");
+var ReactDOM = require("react-dom");
+var App = (function (_super) {
+    __extends(App, _super);
+    function App() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    App.prototype.render = function () {
+        return (React.createElement("div", null,
+            React.createElement("h1", null, "sss")));
+    };
+    return App;
+}(React.Component));
+var appContainer = document.getElementById("app-container");
+ReactDOM.render(React.createElement(App, null), appContainer);
+//# sourceMappingURL=index.js.map
